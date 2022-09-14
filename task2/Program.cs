459e5ce7606dfb56169a2be34d41f1c09858caf6
Dmitriy B <kupperfeet@gmail.com>
2022-09-14 19:13:18 +0300

    task 2 done

---
+++ task2/Program.cs
@@ -0,0 +1,11 @@
+﻿// Найти сумму цифр числа
+
+int DivNumber(int number)
+{
+    if(number == 0) return 0;
+    else return number % 10 + DivNumber(number / 10); 
+}
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine(DivNumber(number));
